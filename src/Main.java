c57ae2962d13c64205bece964296913c1cfa09d3
dzenang <dzenan.granulo@gmail.com>
2024-01-22 11:13:19 +0100

    Add lesson content

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // void method can have return statement
+        if(args[0].equals("exit")) {
+            return;
+        }
+        if (args.length == 0) {
+            String returnedString = nameResolver(null);
+            System.out.println(returnedString);
+            System.out.println(nameResolver("Mickey"));
+        }
+    }
+
+    static String nameResolver(String name) {
+        // name = "Mickey"; is being executed when method called
+        if (name != null) {
+            System.out.println("Length of name is " + name.length());
+        }
+        if (name == "Mickey") {
+            return "The name is " + name;
+        } else if (name == "MickeY") {
+            return "The name is now like this: " + name;
+        }
+
+        return "";
+    }
+}
